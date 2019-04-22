2d00471ab89a1923e4890b98048880c2e93eef4a
Leonardo <leonardo.diniz95@yahoo.com.br>
2019-04-22 16:17:39 -0300

    Sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,42 +1,10 @@
  <!--  CARDS  -->
  <div class="col-md-4 col-sm-12">
+ 
+    <?php   // Barra lateral sendo adicionada
 
-<!--  CATEGORIAS  -->
-<div class="card mb-4">
-    <h5 class="card-header">Últimos posts</h5>
-    <div class="card-body">
-      <ul>
-        <li><a href="">Projetos</a></li>
-        <li><a href="">Novidades</a></li>
-        <li><a href="">Planos</a></li>
-      </ul>
-    </div>
-</div>
+        dynamic_sidebar('Barra lateral');
 
-<!--  ÚLTIMOS POSTS  -->
-<div class="card mb-4">
-<h5 class="card-header">Últimos posts</h5>
-<div class="card-body">
-  <ul>
-    <li><a href="">Lorem ipsum dolor sit.</a></li>
-    <li><a href="">Lorem ipsum dolor sit amet consectetur.</a></li>
-    <li><a href="">Lorem ipsum dolor sit amet.</a></li>
-    <li><a href="">Lorem ipsum dolor sit.</a></li>
-    <li><a href="">Lorem ipsum dolor sit amet consectetur.</a></li>
-  </ul>
-</div>
-</div>
-
-<!--  CATEGORIAS  -->
-<div class="card mb-4">
-  <h5 class="card-header">Links</h5>
-  <div class="card-body">
-    <ul>
-      <li><a href="">Instagram</a></li>
-      <li><a href="">GitHub</a></li>
-      <li><a href="">Facebook</a></li>
-    </ul>
-  </div>
-</div>
+    ?>
 
 </div>
